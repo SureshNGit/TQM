a4db83b5febf86d23b8338ea8950d10bfe25b249
SureshNGit <suresh.subramaniyan@outlook.com>
2022-06-02 01:25:28 +0530

    Implemented ble connectivity 50%

--- TQM/TQM/BluetoothConfig.xaml.cs
+++ TQM/TQM/BluetoothConfig.xaml.cs
@@ -1,6 +1,11 @@
-﻿using Plugin.BluetoothClassic.Abstractions;
+﻿using Android.Bluetooth;
+using Java.IO;
+using Java.Util;
+using Plugin.BluetoothClassic.Abstractions;
 using System;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -11,21 +16,105 @@ namespace TQM
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BluetoothConfig : ContentPage
     {
-        Memory<byte> BufferSize;
+        //Memory<byte> BufferSize;
+
         private CancellationTokenSource cs;
         private IBluetoothConnection connection;
+        private BluetoothSocket _socket;
 
+        [System.Obsolete]
         public BluetoothConfig()
         {
             InitializeComponent();
+            //lv_devices.ItemsSource = outputList;
+            bleConnect();
 
-            FillDevices();
+            //FillDevices();
+        }
+        [System.Obsolete]
+        private async void bleConnect()
+        {
+            BluetoothAdapter adapter = BluetoothAdapter.DefaultAdapter;
+
+            if (adapter == null)
+                Debug.WriteLine("No Bluetooth adapter found.");
+
+            if (!adapter.IsEnabled)
+                Debug.WriteLine("Bluetooth adapter is not enabled.");
+
+
+            BluetoothDevice device = (from bd in adapter.BondedDevices
+                                      where bd.Name == "G85219651634"
+                                      select bd).FirstOrDefault();
+
+            if (device == null)
+                Debug.WriteLine("Named device not found.");
+
+            _socket = device.CreateRfcommSocketToServiceRecord(UUID.FromString("00001101-0000-1000-8000-00805f9b34fb"));
+
+            await _socket.ConnectAsync();
+            Listen(_socket.InputStream);
+        }
+
+
+        private async void Listen(Stream inStream)
+        {
+            bool Listening = true;
+            Debug.WriteLine("Listening has been started.");
+            //byte[] uintBuffer = new byte[sizeof(uint)]; // This reads the first 4 bytes which form an uint that indicates the length of the string message.
+            //byte[] textBuffer; // This will contain the string message.
+            //var readLength = 0;
+            // Keep listening to the InputStream while connected.
+
+            while (Listening)
+            {
+                try
+                {
+                    //textBuffer = new byte[256];
+                    //int read = await inStream.ReadAsync(textBuffer, 0, textBuffer.Length - readLength);
+                    //readLength = readLength + read;
+                    //string s = Encoding.UTF8.GetString(textBuffer);
+                    //Debug.WriteLine("Received: " + s.ToString());
+
+                    var mReader = new InputStreamReader(inStream);
+                    var buffer = new BufferedReader(mReader);
+                    //string output = "";
+                    while (true)
+                    {
+                        if (buffer.Ready())
+                        {
+
+                            String op = await buffer.ReadLineAsync();
+                            Debug.WriteLine("Output: " + op);
+                            lbl_output.Text = op;
+                            //char[] chr = new char[256];
+                            //foreach (char c in chr)
+                            //{
+
+                            //    if (c == '\0')
+                            //        break;
+                            //    output += c;
+                            //}
+
+                        }
+                        //Debug.WriteLine("Output: " + output);
+                    }
+                }
+                catch (Java.IO.IOException e)
+                {
+                    Debug.WriteLine("Error: " + e.Message);
+                    Listening = false;
+                    break;
+                }
+            }
+            Debug.WriteLine("Listening has ended.");
         }
 
         private void FillDevices()
         {
             var adapter = DependencyService.Resolve<IBluetoothAdapter>();
-            lv_devices.ItemsSource = adapter.BondedDevices;
+            //lv_devices.ItemsSource = adapter.BondedDevices;
+
         }
 
         private void lv_devices_ItemSelected(object sender, SelectedItemChangedEventArgs e)
@@ -44,18 +133,24 @@ namespace TQM
         private async void read()
         {
 
-            var buffer = new byte[8192];
-            BufferSize = new Memory<byte>(buffer);
+            byte[] buffer = new byte[256];
+            //BufferSize = new Memory<byte>(buffer);
             await connection.ConnectAsync();
             cs = new CancellationTokenSource();
-            if (connection.DataAvailable)
-            {
-                int response = await connection.ReciveAsync(BufferSize, cs.Token);
-                Debug.WriteLine("Byes Read $$$$$$$$$$$$$$$$$" + response);
-                foreach (char c in BufferSize)
-                {
+            //if (connection.DataAvailable)
+            //{
+            //    int response = await connection.ReciveAsync(buffer, cs.Token);
+            //    Debug.WriteLine("Byes Read $$$$$$$$$$$$$$$$$" + response);
+            //    foreach (byte b in buffer)
+            //    {
+            //        Debug.WriteLine("Bytes..........>" + b.ToString());
+            //    }
+            //}
 
-                }
+            while (connection.DataAvailable)
+            {
+                int response = await connection.ReciveAsync(buffer, cs.Token);
+                Debug.WriteLine("Bytes..........>" + buffer.GetValue(0).ToString());
             }
         }
 
--- TQM/TQM/yarnCount.xaml.cs
+++ TQM/TQM/yarnCount.xaml.cs
@@ -1,5 +1,13 @@
-﻿using System;
-
+﻿using Android.Bluetooth;
+using Java.IO;
+using Java.Util;
+using Plugin.BluetoothClassic.Abstractions;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,6 +16,12 @@ namespace TQM
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class yarnCount : ContentPage
     {
+        private CancellationTokenSource cs;
+        private IBluetoothConnection connection;
+        private BluetoothSocket _socket;
+        BluetoothAdapter adapter;
+        BluetoothDevice device;
+
         public yarnCount()
         {
             InitializeComponent();
@@ -15,37 +29,56 @@ namespace TQM
 
         private async void testYCButton_Clicked(object sender, EventArgs e)
         {
+
+
             //Check serial port communication
             //Check initial value is zero
             //Obtain the weight from weight scale via serial port
-            bool seraialCommuication = true;
-            if (seraialCommuication == true)
+
+            bool blueState = await blueConnect();
+            if (blueState)
             {
-                int initialWeight = 0;
-                if (initialWeight == 0)
+                string balOutput = await Listen();
+                if (balOutput == null)
                 {
-                    string testCount_str = await DisplayPromptAsync("Enter Test Count", "", initialValue: "5", maxLength: 2, keyboard: Keyboard.Numeric);
-                    if (testCount_str == null || testCount_str == "")
-                    {
-                        await DisplayAlert("Alert", "Test count cannot be zero!!!", "OK");
-                        return;
-                    }
-                    int testCount = int.Parse(testCount_str);
-                    Random rnd = new Random();
-                    for (int i = 0; i < testCount; i++)
-                    {
-                        double currentWeigth = rnd.NextDouble();
-                        string currentWeigth_Str = currentWeigth.ToString();
-                        await DisplayAlert(currentWeigth_Str, "", "OK");
-                    }
+                    _socket.Dispose();
+                    device.Dispose();
+                    adapter.Dispose();
+                    await DisplayAlert("Error", "Unable to get weight from weighing machine!!!", "OK");
+                    return;
                 }
                 else
                 {
-                    await DisplayAlert("Alert", "Remove weigth to ensure zero!!!", "OK");
+                    lbl_output.Text = "Stable Output is " + balOutput;
+                    _socket.Dispose();
+                    device.Dispose();
+                    adapter.Dispose();
                 }
+                //int initialWeight = 0;
+                //if (initialWeight == 0)
+                //{
+                //    string testCount_str = await DisplayPromptAsync("Enter Test Count", "", initialValue: "5", maxLength: 2, keyboard: Keyboard.Numeric);
+                //    if (testCount_str == null || testCount_str == "")
+                //    {
+                //        await DisplayAlert("Alert", "Test count cannot be zero!!!", "OK");
+                //        return;
+                //    }
+                //    int testCount = int.Parse(testCount_str);
+                //    for (int i = 0; i < testCount; i++)
+                //    {
+
+                //    }
+                //}
+                //else
+                //{
+                //    await DisplayAlert("Alert", "Remove weigth to ensure zero!!!", "OK");
+                //}
             }
             else
             {
+                _socket.Dispose();
+                device.Dispose();
+                adapter.Dispose();
                 await DisplayAlert("Alert", "Communication Error!!!", "OK");
             }
         }
@@ -53,6 +86,94 @@ namespace TQM
         private void addMachine_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new machinePage());
+        }
+
+        private async Task<bool> blueConnect()
+        {
+            try
+            {
+                adapter = BluetoothAdapter.DefaultAdapter;
+
+                if (adapter == null)
+                    Debug.WriteLine("No Bluetooth adapter found.");
+
+                if (!adapter.IsEnabled)
+                    Debug.WriteLine("Bluetooth adapter is not enabled.");
+
+
+                device = (from bd in adapter.BondedDevices
+                          where bd.Name == "G85219651634"
+                          select bd).FirstOrDefault();
+
+
+                if (device == null)
+                    Debug.WriteLine("Named device not found.");
+
+                _socket = device.CreateRfcommSocketToServiceRecord(UUID.FromString("00001101-0000-1000-8000-00805f9b34fb"));
+                await _socket.ConnectAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+                return false;
+            }
+
+        }
+
+
+        private async Task<string> Listen()
+        {
+            String op = null;
+            Stream inStream = _socket.InputStream;
+            bool Listening = true;
+            Debug.WriteLine("Listening has been started.");
+
+            while (Listening)
+            {
+                try
+                {
+                    var mReader = new InputStreamReader(inStream);
+                    var buffer = new BufferedReader(mReader);
+                    int loopCount = 0;
+                    int stableCount = 0;
+                    while (true)
+                    {
+                        if (buffer.Ready())
+                        {
+
+                            op = await buffer.ReadLineAsync();
+                            Debug.WriteLine("Output: " + op);
+                            if (lbl_output.Text == op)
+                            {
+                                stableCount += 1;
+                                if (stableCount > 10)
+                                {
+                                    return op;
+                                }
+                            }
+                            lbl_output.Text = op;
+
+                        }
+
+                        if (loopCount > 50)
+                        {
+                            return op;
+                        }
+                        loopCount += 1;
+                    }
+                }
+                catch (Java.IO.IOException e)
+                {
+                    Debug.WriteLine("Error: " + e.Message);
+                    Listening = false;
+                    return op;
+                }
+
+            }
+            Debug.WriteLine("Listening has ended.");
+
+            return op;
         }
     }
 }
