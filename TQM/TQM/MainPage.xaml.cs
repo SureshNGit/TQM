4ea62cd9267c1a83aee8f4da9ac0ffc9b64a64c2
SureshNGit <suresh.subramaniyan@outlook.com>
2022-11-08 03:40:08 +0530

    Product activation module completed

--- TQM/TQM/MainPage.xaml.cs
+++ TQM/TQM/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using SQLite;
+﻿using Newtonsoft.Json.Linq;
+using Plugin.Connectivity;
+using RestSharp;
+using SQLite;
 using System;
 using TQM.Model;
 using Xamarin.Forms;
@@ -10,8 +13,9 @@ namespace TQM
         public MainPage()
         {
             InitializeComponent();
-
         }
+
+
 
 
         protected override void OnAppearing()
@@ -27,7 +31,11 @@ namespace TQM
                     UserModel userinfo = conn.Table<UserModel>().FirstOrDefault();
                     if (userinfo == null)
                     {
-                        btn_newUser.IsVisible = true;
+                        bool activation_status = activation();
+                        if (activation_status)
+                        {
+                            btn_newUser.IsVisible = true;
+                        }
                     }
                     else
                     {
@@ -88,7 +96,84 @@ namespace TQM
 
         private void btn_newUser_Clicked(object sender, System.EventArgs e)
         {
-            Navigation.PushAsync(new companyPage());
+            if (btn_newUser.Text == "Register")
+            {
+                Navigation.PushAsync(new companyPage());
+            }
+            else if (btn_newUser.Text == "Activation Pending" || btn_newUser.Text == "Activate")
+            {
+                activation();
+            }
+        }
+
+        private bool activation()
+        {
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                btn_newUser.Text = "Activate";
+                btn_newUser.BackgroundColor = Color.Red;
+                btn_newUser.TextColor = Color.White;
+                DisplayAlert("Attention", "Product Registration Failed. Check your internet connection", "Ok");
+                return true;
+            }
+            else
+            {
+                string serialNo = "";
+                serialNo = Android.Provider.Settings.Secure.GetString(Android.App.Application.Context.ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+                var client = new RestClient("https://myconsoleerp.herokuapp.com/tqm/activate");
+                var request = new RestRequest();
+                request.Method = Method.Post;
+                //request.Timeout = Timeout.Infinite;
+                request.AddParameter("userName", "tqmuser");
+                request.AddParameter("serialNo", serialNo);
+                RestResponse response = client.Execute(request);
+                if (response.IsSuccessful)
+                {
+                    JObject jResponse = JObject.Parse(response.Content);
+                    if (jResponse != null)
+                    {
+                        if (jResponse["status"].ToString() == "Activation Pending")
+                        {
+                            btn_newUser.Text = "Activation Pending";
+                            btn_newUser.BackgroundColor = Color.Yellow;
+                            btn_newUser.TextColor = Color.Black;
+                            DisplayAlert("Attention", "Product activation initiated!!! Contact manufacturer for approval", "Ok");
+                            return true;
+                        }
+                        else if (jResponse["status"].ToString() == "activated")
+                        {
+                            btn_newUser.Text = "Register";
+                            btn_newUser.BackgroundColor = Color.Green;
+                            btn_newUser.TextColor = Color.White;
+                            DisplayAlert("Attention", "Product activation is successful!!!", "Ok");
+                            return true;
+                        }
+                        else if (jResponse["status"].ToString() == "re-activated")
+                        {
+                            btn_newUser.Text = "Register";
+                            btn_newUser.BackgroundColor = Color.Green;
+                            btn_newUser.TextColor = Color.White;
+                            DisplayAlert("Attention", "Product re-activation is successful!!!", "Ok");
+                            return true;
+                        }
+                        else
+                        {
+                            DisplayAlert("Attention", "Product Registration Failed. Contact Manufacturer for activation", "Ok");
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        DisplayAlert("Attention", "Product Registration Failed. Contact Manufacturer for activation", "Ok");
+                        return false;
+                    }
+                }
+                else
+                {
+                    DisplayAlert("Attention", "Product Registration Failed. Contact Manufacturer for activation", "Ok");
+                    return false;
+                }
+            }
         }
     }
 }
