0bf4b3dab9fe454ed44bed6a3ad048b72e10f76b
SureshNGit <suresh.subramaniyan@outlook.com>
2023-02-23 21:54:25 +0530

    Implemented reqesting user permission during app start-up

--- TQM/TQM.Android/MainActivity.cs
+++ TQM/TQM.Android/MainActivity.cs
@@ -1,9 +1,10 @@
-﻿
-
+﻿using Android;
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
+using AndroidX.Core.App;
+using System;
 using System.IO;
 
 namespace TQM.Droid
@@ -20,17 +21,90 @@ namespace TQM.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
 
+            if (!(CheckPermissionGranted(Manifest.Permission.WriteExternalStorage) &&
+                    CheckPermissionGranted(Manifest.Permission.ReadExternalStorage) &&
+                    CheckPermissionGranted(Manifest.Permission.AccessFineLocation) &&
+                    CheckPermissionGranted(Manifest.Permission.AccessCoarseLocation) &&
+                    CheckPermissionGranted(Manifest.Permission.Bluetooth) &&
+                    CheckPermissionGranted(Manifest.Permission.BluetoothAdmin) &&
+                    CheckPermissionGranted(Manifest.Permission.BluetoothScan) &&
+                    CheckPermissionGranted(Manifest.Permission.BluetoothConnect) &&
+                    CheckPermissionGranted(Manifest.Permission.BluetoothAdvertise) &&
+                    CheckPermissionGranted(Manifest.Permission.Internet)))
+            {
+                RequestAllPermission();
+            }
+
             string dbName = "tqm_db.sqlite";
             string folderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
             string fullPath = Path.Combine(folderPath, dbName);
 
             LoadApplication(new App(fullPath));
+
+
+        }
+
+        public bool CheckPermissionGranted(string Permissions)
+        {
+            if (ActivityCompat.CheckSelfPermission(this, Permissions) != Permission.Granted)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+
+        private void RequestAllPermission()
+        {
+            if (ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.WriteExternalStorage) ||
+                ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.ReadExternalStorage) ||
+                ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.AccessFineLocation) ||
+                ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.AccessCoarseLocation) ||
+                ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.Bluetooth) ||
+                ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.BluetoothAdmin) ||
+                ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.BluetoothScan) ||
+                ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.BluetoothConnect) ||
+                ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.BluetoothAdvertise) ||
+                ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.Internet))
+            {
+                ActivityCompat.RequestPermissions(this, new String[] {
+                    Manifest.Permission.WriteExternalStorage,
+                    Manifest.Permission.ReadExternalStorage,
+                    Manifest.Permission.AccessFineLocation,
+                    Manifest.Permission.AccessCoarseLocation,
+                    Manifest.Permission.Bluetooth,
+                    Manifest.Permission.BluetoothAdmin,
+                    Manifest.Permission.BluetoothScan,
+                    Manifest.Permission.BluetoothConnect,
+                    Manifest.Permission.BluetoothAdvertise,
+                    Manifest.Permission.Internet}, 100);
+
+            }
+            else
+            {
+                ActivityCompat.RequestPermissions(this, new String[] {
+                    Manifest.Permission.WriteExternalStorage,
+                    Manifest.Permission.ReadExternalStorage,
+                    Manifest.Permission.AccessFineLocation,
+                    Manifest.Permission.AccessCoarseLocation,
+                    Manifest.Permission.Bluetooth,
+                    Manifest.Permission.BluetoothAdmin,
+                    Manifest.Permission.BluetoothScan,
+                    Manifest.Permission.BluetoothConnect,
+                    Manifest.Permission.BluetoothAdvertise,
+                    Manifest.Permission.Internet}, 100);
+            }
         }
     }
 }
