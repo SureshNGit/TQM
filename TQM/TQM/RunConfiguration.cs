6a28ecf14ecdc2d9770ca93484fea82eee7b429c
SureshNGit <suresh.subramaniyan@outlook.com>
2023-03-07 10:54:49 +0530

    Changes in runconfig

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: G85222825615
-        private string balanceSerialNo = "G85222825615";
-        private string tqmAppUserID = "dineshkumarunit1";
+        private string balanceSerialNo = "DS85222849197";
+        private string tqmAppUserID = "tqmuser";
 
         public string getBalanceSerialNo()
         {
