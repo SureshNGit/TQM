e52d4ad7929bd52887f6310beb7393a462d01f09
SureshNGit <suresh.subramaniyan@outlook.com>
2023-04-10 18:47:15 +0530

    Config changes for mothi spinners

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: G85223856821
-        private string balanceSerialNo = "G85223856821";
-        private string tqmAppUserID = "tqmuser";
+        private string balanceSerialNo = "G85223856820";
+        private string tqmAppUserID = "mothiunit1";
 
         public string getBalanceSerialNo()
         {
