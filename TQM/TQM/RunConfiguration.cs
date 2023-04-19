7b0df041c0b1abdb0a6be513fbd5119a99f74d93
SureshNGit <suresh.subramaniyan@outlook.com>
2023-04-19 10:25:41 +0530

    Config changes for demo and mothi

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: G85223856821
-        private string balanceSerialNo = "G85223849846";
-        private string tqmAppUserID = "bhagirathtex2";
+        private string balanceSerialNo = "G85219651634";
+        private string tqmAppUserID = "tqmuser";
 
         public string getBalanceSerialNo()
         {
