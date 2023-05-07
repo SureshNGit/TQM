341f0987f5dad3deae819fb634b0cd163a25d823
SureshNGit <suresh.subramaniyan@outlook.com>
2023-05-07 13:30:44 +0530

    config changes for lan spin tex

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: G85223856821
-        private string balanceSerialNo = "G85223849846";
-        private string tqmAppUserID = "bhagirathtex2";
+        private string balanceSerialNo = "G85223861691";
+        private string tqmAppUserID = "lanspintexunit1";
 
         public string getBalanceSerialNo()
         {
