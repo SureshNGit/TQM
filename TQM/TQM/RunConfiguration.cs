71e632a08b56a089e72da00de14644d68ca066d9
SureshNGit <suresh.subramaniyan@outlook.com>
2023-05-15 16:40:28 +0530

    Config changes for tqmuser

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,9 +4,9 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849197
-        private string balanceSerialNo = "DS85222849197";
+        private string balanceSerialNo = "G85219651634";
         private string loadCellSerailNo = "SASTHA-01";
-        private string tqmAppUserID = "sakthidaranunit1csp";
+        private string tqmAppUserID = "tqmuser";
 
         public string getBalanceSerialNo()
         {
