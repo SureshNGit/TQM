feb219fd49ce23351746f25fa72e0b435a21ef12
SureshNGit <suresh.subramaniyan@outlook.com>
2023-05-20 12:42:41 +0530

    Config changes for Demo Test for Sakthidharan

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -3,9 +3,10 @@
     internal class RunConfiguration
     {
         //Demo balance serail no: G85219651634
+        //Demo loadCellSerailNo: "SASTHA-01";
         //Test balance serail no: DS85222849197
         private string balanceSerialNo = "G85219651634";
-        private string loadCellSerailNo = "SASTHA-01";
+        private string loadCellSerailNo = "INTEL";
         private string tqmAppUserID = "tqmuser";
 
         public string getBalanceSerialNo()
