8c385b61cce59f1b5071dbd0943ff912cacfdeaa
suresh.subramaniyan <suresh.subramaniyan@outlook.com>
2023-08-17 12:19:24 +0530

    Config changes for Macbook and lucky unit 1A APK

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "G85223865598";
-        private string tqmAppUserID = "jagannathaoeunit";
+        private string balanceSerialNo = "G85219647296";
+        private string tqmAppUserID = "lytunit1a";
 
         public string getBalanceSerialNo()
         {
