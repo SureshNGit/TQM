ed5ca12efa47371c15981a4cee219e63f71ca63d
suresh.subramaniyan <suresh.subramaniyan@outlook.com>
2023-09-01 16:16:43 +0530

    Config changes for SriJayajothi

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "DS85222849198";
-        private string tqmAppUserID = "tqmuser";
+        private string balanceSerialNo = "G852223867538";
+        private string tqmAppUserID = "srijayajothiunit1";
         private bool requireCSV = true;
 
         public string getBalanceSerialNo()
