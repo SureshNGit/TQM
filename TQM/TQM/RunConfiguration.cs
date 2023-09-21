c3d88233954a3c828a8ef8e2e6bb62e9941238b8
suresh.subramaniyan <suresh.subramaniyan@outlook.com>
2023-09-21 16:47:06 +0530

    Changes for Developement

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,9 +4,9 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "85214474127";
-        private string tqmAppUserID = "pandiantexunit1";
-        private bool requireCSV = false;
+        private string balanceSerialNo = "G85219651634";
+        private string tqmAppUserID = "tqmuser";
+        private bool requireCSV = true;
 
         public string getBalanceSerialNo()
         {
