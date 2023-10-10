ea43d6c05f72ef03b2e8d47fe84b29ff8d1a702a
suresh.subramaniyan <suresh.subramaniyan@outlook.com>
2023-10-10 17:34:23 +0530

    Config changes for Dharanidara db extract

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "85214474127";
-        private string tqmAppUserID = "pandiantexunit1";
+        private string balanceSerialNo = "G85223856821";
+        private string tqmAppUserID = "dharanidaraunit1";
         private bool requireCSV = false;
 
         public string getBalanceSerialNo()
