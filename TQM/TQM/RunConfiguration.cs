dad624e421efdf20eaf13bbdfeb6be44bea964b5
suresh.subramaniyan <suresh.subramaniyan@outlook.com>
2023-10-28 17:37:48 +0530

    Config changes for Kumaragiri

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "G85223874988";
-        private string tqmAppUserID = "beoneunit1";
+        private string balanceSerialNo = "G85223879656";
+        private string tqmAppUserID = "kumaragiriunit1";
         private bool requireCSV = false;
 
         public string getBalanceSerialNo()
