d52fa5375a18686190a34e769f78fb2e469dabad
suresh.subramaniyan <suresh.subramaniyan@outlook.com>
2023-11-27 17:24:18 +0530

    Configuration update for luckyweavessunit1

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "G85223879656";
-        private string tqmAppUserID = "kumaragiriunit1";
+        private string balanceSerialNo = "G85223878399";
+        private string tqmAppUserID = "luckyweavessunit1";
         private bool requireCSV = false;
 
         public string getBalanceSerialNo()
