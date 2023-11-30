da58c86326876af0848acbd69df408fe88b7f3d8
suresh.subramaniyan <suresh.subramaniyan@outlook.com>
2023-11-30 09:45:37 +0530

    Balance serial no change for srijayajothiunit1

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "DS85222849198";
-        private string tqmAppUserID = "tqmuser";
+        private string balanceSerialNo = "G85223865602";
+        private string tqmAppUserID = "srijayajothiunit1";
         private bool requireCSV = false;
 
         public string getBalanceSerialNo()
