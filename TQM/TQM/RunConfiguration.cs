fb1b4b8419408b16b40e5068e7884dd884b99640
suresh.subramaniyan <suresh.subramaniyan@outlook.com>
2023-12-11 18:44:38 +0530

    Config changes for svyakumaragiri trail

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "Infotest";
-        private string tqmAppUserID = "svyaluckyunit1a";
+        private string balanceSerialNo = "SS-01";
+        private string tqmAppUserID = "svyakumaragiri";
         private bool requireCSV = false;
 
         public string getBalanceSerialNo()
