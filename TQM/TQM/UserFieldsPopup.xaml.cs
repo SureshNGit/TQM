c8fc6c35b5401ed9b944dadb4ef586b0e96cee5d
suresh.subramaniyan <suresh.subramaniyan@outlook.com>
2023-12-14 14:08:28 +0530

    Added userfield pop-up

---
+++ TQM/TQM/UserFieldsPopup.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.CommunityToolkit.UI.Views;
+
+namespace TQM
+{	
+	public partial class UserFieldsPopup : Popup
+    {	
+		public UserFieldsPopup ()
+		{
+			InitializeComponent ();
+		}
+
+
+        private void btn_confirm_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+
+
+                
+                Dismiss("Success~" + entry_UF1.ToString() + "|~*~|" + entry_UF2.ToString() + "|~*~|" + entry_UF3.ToString() + "|~*~|" + entry_UF4.ToString());
+            }
+            catch (Exception)
+            {
+                Dismiss("Error occurred!!! Try again...");
+            }
+        }
+
+        
+    }
+
+
+}
+
