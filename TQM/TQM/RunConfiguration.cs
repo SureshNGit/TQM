94061194ec7e1f75dc7aed2e88b53b7b06588e08
SureshNGit <82797223+SureshNGit@users.noreply.github.com>
2024-03-26 15:45:37 +0530

    Config changes to extract LS mill Unit1 DB and for Win11 Macbook

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "G85223891414";
-        private string tqmAppUserID = "lsmillsunita";
+        private string balanceSerialNo = "G85223874667";
+        private string tqmAppUserID = "lsmillsunit1";
         private bool requireCSV = false;
 
         public string getBalanceSerialNo()
