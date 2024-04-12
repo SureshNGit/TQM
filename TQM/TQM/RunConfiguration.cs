4c5abd3b032a2ac7dca1df93ba0ce2b62024494a
SureshNGit <82797223+SureshNGit@users.noreply.github.com>
2024-04-12 16:02:52 +0530

    Config changes for Shristi Cotspin Unit-2

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "G85222792761";
-        private string tqmAppUserID = "lytunit1";
+        private string balanceSerialNo = "G85223891413";
+        private string tqmAppUserID = "shristiunit2";
         private bool requireCSV = false;
 
         public string getBalanceSerialNo()
