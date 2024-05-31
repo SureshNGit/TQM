ab86c528e46be9b5f2cadeed9db0e5a0bfdc7673
SureshNGit <82797223+SureshNGit@users.noreply.github.com>
2024-05-31 12:38:34 +0530

    Config changes for jayalakshmiunit1

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,7 +4,7 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "G85219651634";
+        private string balanceSerialNo = "G85223859592";
         private string tqmAppUserID = "jayalakshmiunit1";
         private bool requireCSV = false;
 
