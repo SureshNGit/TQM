b36ed67b79df8586668289a4d7158a7df0b8a893
SureshNGit <82797223+SureshNGit@users.noreply.github.com>
2024-06-15 13:17:55 +0530

    Config changes for Expo'24

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "G85223860077";
-        private string tqmAppUserID = "lsspinning";
+        private string balanceSerialNo = "H2400011934";
+        private string tqmAppUserID = "tqmuser";
         private bool requireCSV = false;
 
         public string getBalanceSerialNo()
