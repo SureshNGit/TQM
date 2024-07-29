c7577a32df54b514b67c3f39a64fde665db28232
SureshNGit <82797223+SureshNGit@users.noreply.github.com>
2024-07-29 12:44:11 +0530

    Config changes for KP Unit-1

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "G85223859593";
-        private string tqmAppUserID = "thenpandianspg";
+        private string balanceSerialNo = "H2400011934";
+        private string tqmAppUserID = "kptunit1";
         private bool requireCSV = false;
 
         public string getBalanceSerialNo()
