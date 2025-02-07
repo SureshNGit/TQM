7d904b1df9a9451cc9f961f4e0224ae9e061e7ab
SureshNGit <82797223+SureshNGit@users.noreply.github.com>
2025-02-07 14:00:29 +0530

    Config changes for luckyweavessunit2

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "H2400023957";
-        private string tqmAppUserID = "srijayajothiunit1";
+        private string balanceSerialNo = "H2500064124";
+        private string tqmAppUserID = "luckyweavessunit2";
         private bool requireCSV = false;
         private bool noilsAutoCorrection = false;
         private bool apercentAutoCorrection = false;
