62aeab3986f0746e45f6893a9d80d0dca2cbec77
SureshNGit <82797223+SureshNGit@users.noreply.github.com>
2025-04-08 11:04:13 +0530

    Config changes for cheranspg

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "H250074786";
-        private string tqmAppUserID = "sricheran";
+        private string balanceSerialNo = "H2500064126";
+        private string tqmAppUserID = "cheranspg";
         private bool requireCSV = false;
         private bool noilsAutoCorrection = false;
         private bool apercentAutoCorrection = false;
