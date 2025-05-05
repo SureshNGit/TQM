93e971ddaa2ad96037153ef104a1765a847ee226
SureshNGit <82797223+SureshNGit@users.noreply.github.com>
2025-05-05 13:04:43 +0530

    Config changes for subburaajunit1

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "G85223874988";
-        private string tqmAppUserID = "beoneunit1";
+        private string balanceSerialNo = "G85223865600";
+        private string tqmAppUserID = "subburaajunit1";
         private bool requireCSV = false;
         private bool noilsAutoCorrection = false;
         private bool apercentAutoCorrection = false;
