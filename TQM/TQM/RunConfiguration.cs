cf9cd7d0653984aba15de40285f58dd6cb68fc28
SureshNGit <82797223+SureshNGit@users.noreply.github.com>
2025-05-20 12:27:06 +0530

    Config changes for lsmillsunit1

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "G85222849196";
-        private string tqmAppUserID = "jagannathaunit3";
+        private string balanceSerialNo = "G85223874667";
+        private string tqmAppUserID = "lsmillsunit1";
         private bool requireCSV = false;
         private bool noilsAutoCorrection = false;
         private bool apercentAutoCorrection = false;
