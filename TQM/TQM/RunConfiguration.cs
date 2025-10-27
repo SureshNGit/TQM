36a5e856cb9ad1c490a4b1b5d259b0654d51527f
SureshNGit <82797223+SureshNGit@users.noreply.github.com>
2025-10-27 07:48:52 -0400

    Config changes for prabhuoe1

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "H2500119624";
-        private string tqmAppUserID = "motherland";
+        private string balanceSerialNo = "H2500119620";
+        private string tqmAppUserID = "prabhuoe1";
         private bool moveReportToCloud = true;
         private bool requireCSV = false;
         private bool noilsAutoCorrection = false;
