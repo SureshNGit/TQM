5bbb5a3beed75f3534715713b7b890e7cc65f35e
SureshNGit <82797223+SureshNGit@users.noreply.github.com>
2025-11-10 01:12:03 -0500

    Config changes for CSP demo

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -5,9 +5,9 @@
         //Demo balance serail no: G85219651634
         //Demo loadCellSerailNo: "SASTHA-01";
         //Test balance serail no: DS85222849197
-        private string balanceSerialNo = "DS85222849197";
-        private string loadCellSerailNo = "INTEL";
-        private string tqmAppUserID = "sakthidaranunit1csp";
+        private string balanceSerialNo = "G85223867538";
+        private string loadCellSerailNo = "SASTHA-02";
+        private string tqmAppUserID = "tqmuser";
 
         public string getBalanceSerialNo()
         {
