52cee7f65479f6b923f848d38be9cce24bb3a959
SureshNGit <82797223+SureshNGit@users.noreply.github.com>
2026-04-24 19:08:17 -0400

    Config update for User ID - svyaluckyunit1b

--- TQM/TQM/RunConfiguration.cs
+++ TQM/TQM/RunConfiguration.cs
@@ -4,8 +4,8 @@
     {
         //Demo balance serail no: G85219651634
         //Test balance serail no: DS85222849198
-        private string balanceSerialNo = "SS-07";
-        private string tqmAppUserID = "svyapallava";
+        private string balanceSerialNo = "SS-02";
+        private string tqmAppUserID = "svyaluckyunit1b";
         private bool requireCSV = false;
 
         public string getBalanceSerialNo()
